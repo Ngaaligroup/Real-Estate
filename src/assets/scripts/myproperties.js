f13e5ed0c19ea8ba996388f42b1d206217d20b7d
Shirley Liz <shirleymbabazi@gmail.com>
2020-06-30 12:55:37 +0300

    src folder missing

---
+++ src/assets/scripts/myproperties.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+	firebase.auth().onAuthStateChanged(function(user) {
+	    if (user) {
+	         var uid = user.uid;
+	         console.log(uid);
+
+			// var uid = firebase.auth().currentUser.uid;
+			var ref = firebase.database().ref("users/" +uid+ "/property/");
+			ref.once("value").then(function(snapshot){
+		    	$('#tts').html('');
+		    	snapshot.forEach(function(childSnapshot) {
+		    		var vl = childSnapshot.val();
+			    	var key = childSnapshot.key;
+			    	console.log(vl);
+
+			    	var Title=vl.Title;
+			        var Address=vl.Address;
+			        var Area=vl.Area;
+			        var description=vl.Description;
+			        var price=vl.Price;
+			        var Land_Type=vl.Land_Type;
+			        var Photos=vl.Photos;
+			        var Status=vl.Status;
+			        var bed=vl.Beds;
+			        var baths=vl.Bathrooms;
+			        var garage=vl.Garages;
+			        var date=vl.Date;
+			        
+
+			        $('#tts').
+			        append(
+			        	'<tr>'+
+			                '<td class="image">'+
+			                    '<a href="property-detail.html?name='+key+'"><img alt="property picture" src="'+Photos+'"></a>'+
+			                '</td>'+
+			                '<td><div class="inner">'+
+			                    '<a href="property-detail.html?name='+key+'"><h2>'+Title+'</h2></a>'+
+			                    '<figure>'+Address+'</figure>'+
+			                    '<div class="tag price">UGX '+price+'</div>'+
+			                '</div>'+
+			                '</td>'+
+			                '<td>'+date+'</td>'+
+			                '<td>236</td>'+
+			                '<td class="actions">'+
+			                    '<a href="#" class="edit" id="edits"  ><i class="fa fa-pencil"></i>Edit</a>'+
+			                    '<a href="#" id="deletes" "><i class="delete fa fa-trash-o"></i></a>'+
+			                '</td>'+
+		            	'</tr>'
+
+			        	);
+			        $('#edits').click(function(){
+						window.open("update.html?name="+key+"");
+						$('#submit-title').attr('placeholder',+Title);
+    
+					});
+					$('#deletes').click(function(){
+						
+						var refDB = firebase.database().ref("users/" + uid+ "/property/" + key);
+						refDB.remove()
+						.then(function() {
+						//   console.log("Remove succeeded.")
+						})
+						.catch(function(error) {
+						  console.log("Remove failed: " + error.message);
+						});
+
+						var ref2 = firebase.database().ref("AllProperty/" +key);
+						ref2.remove()
+						.then(function() {
+						//   console.log("Remove succeeded.")
+						})
+						.catch(function(error) {
+						  console.log("Remove failed: " + error.message);
+						});
+
+						var ref3 = firebase.database().ref("properties/Land/" +key);
+						ref3.remove()
+						.then(function() {
+						//   console.log("Remove succeeded.")
+						})
+						.catch(function(error) {
+						  console.log("Remove failed: " + error.message);
+						});
+
+						var ref4 = firebase.database().ref("properties/house/" +key);
+						ref4.remove()
+						.then(function() {
+						  console.log("Remove succeeded.");
+						})
+						.catch(function(error) {
+						  console.log("Remove failed: " + error.message);
+						});
+
+						window.open("submit.html");
+
+		            });
+
+
+    
+					});
+			       
+			});
+		}else{
+			window.alert("you not authorized");
+		}
+		
+	});
+});
---
+++ src/assets/scripts/userdetails.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+  
+function getUrlParam(param)
+            {
+              param = param.replace(/([\[\](){}*?+^$.\\|])/g, "\\$1");
+              var regex = new RegExp("[?&]" + param + "=([^&#]*)");
+              var url   = decodeURIComponent(window.location.href);
+              var match = regex.exec(url);
+              return match ? match[1] : "";
+
+              
+            }
+        var param = getUrlParam("name");
+        var ref = firebase.database().ref("users/" +param);
+        ref.once("value").then(function(snapshot) {
+            var val = snapshot.val();
+            console.log(val);
+            var Fname=snapshot.val().FirstName;
+	        var Lname=snapshot.val().LastName;
+	        var email=val.email;
+	        var phone=val.phone;
+	        var aboutme=val.AboutMe;
+	        var propertiesNumber=snapshot.child("property").numChildren();
+
+	        $('#usernames').append('<header><h1 >'+Fname+' '+Lname+'</h1></header>');
+	        document.getElementById('phne').innerHTML = phone;
+	        document.getElementById('phnes').innerHTML = phone;
+	        $('.useremail').append('<a href="mailto:'+email+'">'+email+'');
+	        if (aboutme=="undefined") {
+	        	document.getElementById('aboutme').innerHTML="";
+	        }else{
+	        	document.getElementById('aboutme').innerHTML="aboutme";
+	        }
+	        $('.countprop').append('<header><h3>My Properties ('+propertiesNumber+')</h3></header>');
+	        
+
+	    });
+	    var propertiesref= firebase.database().ref("users/" +param+ "/property/");
+	    propertiesref.once("value").then(function(snapshot){
+	    	$('#userpropert').html('');
+	    	snapshot.forEach(function(childSnapshot) {
+	    	
+		    	var vl = childSnapshot.val();
+		    	var key = childSnapshot.key;
+		    	
+
+		    	var Title=vl.Title;
+		        var Address=vl.Address;
+		        var Area=vl.Area;
+		        var description=vl.Description;
+		        var price=vl.Price;
+		        var Land_Type=vl.Land_Type;
+		        var Photos=vl.Photos;
+		        var Status=vl.Status;
+		        var bed=vl.Beds;
+		        var baths=vl.Bathrooms;
+		        var garage=vl.Garages;
+
+		        
+
+
+		    	$('#userpropert')
+		    	.append(
+		    		'<div class="col-md-4 col-sm-4">'+
+	                    '<div class="property">'+
+	                        '<a href="property-detail.html?name='+key+'" >'+
+	                            '<div class="property-image">'+
+	                                '<img alt="" src="'+Photos+'">'+
+	                            '</div>'+
+	                            '<div class="overlay">'+
+	                                '<div class="info">'+
+	                                    '<div class="tag price">UGX '+price+'</div>'+
+	                                    '<h3>'+Title+'</h3>'+
+	                                    '<figure>'+Address+'</figure>'+
+	                                '</div>'+
+	                                '<ul class="additional-info">'+
+	                                    '<li>'+
+	                                        '<header>Area:</header>'+
+	                                        '<figure><span >'+Area+ '</span>m<sup>2</sup></figure>'+
+	                                    '</li>'+
+	                                    '<li>'+
+	                                        '<header>Beds:</header>'+
+	                                        '<figure>'+bed+'</figure>'+
+	                                    '</li>'+
+	                                    '<li>'+
+	                                        '<header>Baths:</header>'+
+	                                        '<figure>'+baths+'</figure>'+
+	                                    '</li>'+
+	                                    '<li>'+
+	                                        '<header>Garages:</header>'+
+	                                        '<figure>'+garage+'</figure>'+
+	                                    '</li>'+
+	                                '</ul>'+
+	                            '</div>'+
+	                        '</a>'+
+	                    '</div>'+
+	                '</div>'
+		    		);
+		    });
+	    });
+});
