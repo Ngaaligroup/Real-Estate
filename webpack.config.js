04cd21c9dafc73320f151fc322443523ca71f6ea
KaluleKevin96 <kalulekevin96@gmail.com>
2020-07-05 01:32:56 +0300

    Added PNG to the allowed types for images and finished implementing the navbar change bg color effect

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|PNG)$/,
         use: ['file-loader'],
       },
     ]
